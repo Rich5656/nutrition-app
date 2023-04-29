678d9c3745b039e415000c383709988ef57b3261
Rich5656 <richard.k.lowther@gmail.com>
2023-04-29 17:00:49 -0400

    Fixed issue preventing netlify deployment in the reducer

--- src/context/AppReducer.jsx
+++ src/context/AppReducer.jsx
@@ -1,4 +1,4 @@
-export default (state, action) => {
+const reducer =  (state, action) => {
     switch(action.type) {
         case 'DELETE_FOOD':
             return {
@@ -28,3 +28,5 @@ export default (state, action) => {
             return state;
     }
 }
+
+export default reducer;
